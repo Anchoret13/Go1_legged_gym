bcbc7970532032875e7b146de11b35d0c56d04ec
Anchoret13 <vergil1133@live.com>
2024-04-08 09:49:28 -0500

    bug fixed

--- legged_gym/envs/go1_fw_clock/go1_fw.py
+++ legged_gym/envs/go1_fw_clock/go1_fw.py
@@ -578,10 +578,10 @@ class Go1FwClock(WheeledRobot):
             return 0.
         
     def _reward_feet_clearance(self):
-        # only rear feet
+        # NOTE: based on phase, not working as expected?
         phases = 1 - torch.abs(1.0 - torch.clip((self.rear_foot_indices * 2.0) - 1.0, 0.0, 1.0) * 2.0)
         foot_height = (self.rear_foot_positions[:, :, 2]).view(self.num_envs, -1)
-        target_height = 0.02 * phases + 0.02 # currently target height is 0.02
+        target_height = 0.04 * phases + 0.04 # currently target height is 0.02
         rew_foot_clearance = torch.square(target_height - foot_height) * (1 - self.desired_rear_contact_states)
         return torch.sum(rew_foot_clearance, dim=1)
 
